4960ed93c548ceedac01d56beec81376994358a3
demi-Ad <votm777@naver.com>
2022-02-25 09:16:01 +0900

    MockUpData 폴더 생성 추가

--- src/main/java/com/study/board/web/util/StartUpMockDataInit.java
+++ src/main/java/com/study/board/web/util/StartUpMockDataInit.java
@@ -5,9 +5,13 @@ import com.study.board.domain.entity.post.repository.PostRepository;
 import com.study.board.domain.entity.user.User;
 import com.study.board.domain.entity.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
 
 @Component
 @RequiredArgsConstructor
@@ -15,6 +19,14 @@ public class StartUpMockDataInit implements ApplicationListener<ContextRefreshed
 
     private final PostRepository postRepository;
     private final UserRepository userRepository;
+
+    @PostConstruct
+    private void init() {
+        File file = new File(Constants.IMAGE_PATH);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
