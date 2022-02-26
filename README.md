8177e7864c1a0d4bb9797d38dc1e3ba795b3c6e1
Demi-Ad <ttpp1212@@>
2022-02-26 15:42:58 +0900

    README 수정

--- README.md
+++ README.md
@@ -6,10 +6,28 @@
 > ### [BoardApplication.class](/src/main/java/com/study/board/BoardApplication.java) 
 > RUN Main Class
 
-
+---
 ### Prerequisites / 선행 조건
 
 ```
 JAVA 11
-Gradle Build
 ```
+---
+### Build
+Location = [gradlew](/gradlew)
+```shell
+gradelw build
+```
+> ### args = {-x test} 테스트 생략
+> ### output = /build/libs/*.jar
+
+---
+
+### 참고 사항
+> ### [application-mysql.yml](src/main/resources/application-mysql.yml)
+> ### mysql 사용시 해당 프로필 이용
+> ### 기본값 : 메모리 H2 데이터베이스
+```shell 
+java -jar -Dspring.profiles.active={profile} *.jar
+```
+
--- src/test/java/com/study/board/domain/entity/user/repository/UserRepositoryTest.java
+++ src/test/java/com/study/board/domain/entity/user/repository/UserRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.study.board.domain.entity.user.repository;
 
 import com.study.board.domain.entity.user.User;
-import com.study.board.domain.entity.user.projection.UserProjection;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -23,19 +18,6 @@ class UserRepositoryTest {
 
     @Autowired
     EntityManager em;
-
-    @Test
-    void findByUserIdAndPassword() {
-
-        User save = repository.save(new User("a", "aa", "aa", "a@a"));
-
-        em.flush();
-        em.clear();
-
-        List<UserProjection> projectionAll = repository.findAllBy();
-        Assertions.assertThat(projectionAll.size()).isEqualTo(1);
-        Assertions.assertThat(projectionAll.get(0).getUserId()).isEqualTo(save.getUserId());
-    }
 
     @Test
     void findByUserIdExists() {
